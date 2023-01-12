a38621488aff3a640e01c5ec072278860a8b80dc
Oluwaseun <ruliseun@gmail.com>
2023-01-12 22:12:39 +0100

    logic update and refactoring

--- dist/email/emailVerification.js
+++ dist/email/emailVerification.js
@@ -6,7 +6,7 @@ function emailVerificationView(token) {
     let temp = `
      <div style="max-width: 700px;
      margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-     <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Airtime to Cash POD G.</h2>
+     <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Airtime to Cash App</h2>
       <p>Hi there, Follow the link by clicking on the button to verify your email
       </p>
        <a href=${link}
